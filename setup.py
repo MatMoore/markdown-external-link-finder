0d0d2efff92f197cc57f7e8d8f22fc16747268be
Mat Moore <matmoore@users.noreply.github.com>
2019-05-27 21:51:56 +0100

    ???

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='markdown_external_link_finder',
-      version='0.1',
+      version='0.0.1',
       description='Finds external links in markdown files',
       url='http://github.com/matmoore/markdown-external-link-finder',
       license='MIT',
