69d8d48b06ab90ea847083f84a5d616a379708e5
Mat Moore <matmoore@users.noreply.github.com>
2019-05-27 20:31:17 +0100

    Mess around with circleci

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(name='markdown_external_link_finder',
       version='0.1',
       description='Finds external links in markdown files',
       url='http://github.com/matmoore/markdown-external-link-finder',
-      author='Mat Moore',
-      author_email='MatMoore@users.noreply.github.com',
       license='MIT',
       packages=['markdown_external_link_finder'],
-      zip_safe=False)
+      python_requires='>=3.6',
+      install_requires='mistune')
